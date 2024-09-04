3f17bf1a7fd5bb09b6a448f3f103f022726683cc
colleenhand <chand02@saintmarys.edu>
2024-09-04 17:00:30 -0400

    Add files via upload

---
+++ L4.py
@@ -0,0 +1,8 @@
+#the authors' names are: Colleen and Lily
+import math
+radius = 4
+length = 8 / math.sqrt(2)
+circle = math.pi * radius ** 2
+square = length * length
+area = circle - square
+print(area)
